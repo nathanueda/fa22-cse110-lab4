82b46562f4afcf4ad71e9dbc339d656ed56a2745
Nathan Ueda <nateueda@gmail.com>
2022-10-23 15:43:02 -0700

    Changed print value for q16.

--- expose/javascript/part2-question16.js
+++ expose/javascript/part2-question16.js
@@ -1,9 +1,5 @@
 for (let car in statistics) {
-    if (car[0] == 'r') {
-        console.log(car);
-    }
-    
-    if (statistics[car] % 2 != 0) {
+    if (car[0] == 'r' || statistics[car] % 2 != 0) {
         console.log(statistics[car]);
     }
 }
